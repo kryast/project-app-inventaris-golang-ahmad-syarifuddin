6c43ce7d5387909e8479df57683685856f81e659
Ahmad Syarifuddin <oden.psht@gmail.com>
2024-11-08 16:07:15 +0700

    create item

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module project-app-inventaris-golang-ahmad-syarifuddin
 
 go 1.23.2
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/lib/pq v1.10.9
+)
--- main.go
+++ main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+	"project-app-inventaris-golang-ahmad-syarifuddin/database"
+	"project-app-inventaris-golang-ahmad-syarifuddin/handler"
+	"project-app-inventaris-golang-ahmad-syarifuddin/repository"
+	"project-app-inventaris-golang-ahmad-syarifuddin/service"
+
+	"github.com/go-chi/chi"
+)
+
+func main() {
+	db, err := database.InitDB()
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	repoItem := repository.NewItemRepository(db)
+	serviceItem := service.NewItemService(repoItem)
+	itemHandler := handler.NewItemHandler(serviceItem)
+
+	r := chi.NewRouter()
+
+	// Rute untuk User
+	r.Post("/create", itemHandler.CreateItemHandler)
+
+	fmt.Println("Server started on port 8080")
+	http.ListenAndServe(":8080", r)
+
+}
