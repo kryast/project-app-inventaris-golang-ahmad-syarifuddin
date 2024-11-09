06bd2b51a33d409fb9cdf16bad60ab4d7d1410e5
Ahmad Syarifuddin <oden.psht@gmail.com>
2024-11-09 18:50:05 +0700

    get all items

--- model/item.go
+++ model/item.go
@@ -6,10 +6,10 @@ type Item struct {
 	ID                  int       `json:"id"`
 	Name                string    `json:"name"`
 	CategoryID          int       `json:"category_id"`
-	CategoryName        string    `json:"category_name,omitempty"`
+	CategoryName        string    `json:"category_name"`
 	PhotoURL            string    `json:"photo_url"`
 	Price               int       `json:"price"`
 	PurchaseDate        time.Time `json:"purchase_date"`
-	TotalUsageDay       int       `json:"total_usage_day"`
+	TotalUsageDay       int       `json:"total_usage_days"`
 	ReplacementRequired bool      `json:"replacement_required"`
 }
